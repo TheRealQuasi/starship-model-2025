b950f05b40606faa4554e1a3a8fae0a04ae221c8
Fredrik Sandén <fred.sanden@gmail.com>
2025-04-16 14:04:20 +0200

    Changed interupt pin to pin 9 an pin to input_pullup. renamed flow to flow_PWM3901 to not confuse with Sensor_handler flow varaible

--- Starship_25/lib/Optical_flow/Optical_flow.cpp
+++ Starship_25/lib/Optical_flow/Optical_flow.cpp
@@ -4,8 +4,8 @@
 volatile bool flowDataReady = false; // Flag to indicate if new data is available
 
 // Using digital pin 10 for chip select
-Bitcraze_PMW3901 flow(10); // SPI CS pin 10 for optical flow sensor
-// MOSI: 11, MISO: 12, SCK: 13, INT: 2
+Bitcraze_PMW3901 flow_PMW3901(10); // SPI CS pin 10 for optical flow sensor
+// MOSI: 11, MISO: 12, SCK: 13, INT: 9
 
  // Interrupt service routine for optical flow sensor
 void flowISR() {
@@ -13,22 +13,22 @@ void flowISR() {
 }
 
 void setupFlowISR() {
-    pinMode(PMW3901_INT_PIN, INPUT); // Set the interrupt pin as input
-    attachInterrupt(digitalPinToInterrupt(PMW3901_INT_PIN), flowISR, RISING); // Attach interrupt on rising edge
+    pinMode(PMW3901_INT_PIN, INPUT_PULLUP); // Set the interrupt pin as input with internal pull-up resistor
+    attachInterrupt(digitalPinToInterrupt(PMW3901_INT_PIN), flowISR, FALLING); // Attach interrupt on faling edge
 }
 
 bool Optical_flow::begin() {
     // Set up the interrupt for the optical flow sensor
     setupFlowISR();
     Serial.println("Optical flow sensor interrupt initiated.");
-    return flow.begin();
+    delay(50);
+    return flow_PMW3901.begin();
 }
 
 bool Optical_flow::readMotion( int16_t &deltaX, int16_t &deltaY) { //, int16_t &qual)
     // Get motion count since last call
-    flow.readMotionCount(&deltaX, &deltaY);
+    flow_PMW3901.readMotionCount(&deltaX, &deltaY);
 
-    delay(100);
     return true;
 }
  
